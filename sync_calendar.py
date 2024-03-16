a4db2cfc55c4fa6eb36c5fe2d45df1bab88da950
Huzefa Dargahwala <vizkid2005@gmail.com>
2024-03-16 15:24:23 -0700

    Working version from prod, no merging

--- sync_calendar.py
+++ sync_calendar.py
@@ -1,46 +1,40 @@
 from gcsa.event import Event
 from gcsa.google_calendar import GoogleCalendar
 from google.oauth2.credentials import Credentials
-from datetime import datetime, timezone
+from datetime import datetime
 import re
 import argparse
 import yaml
 from requests import get, post
 import json
-from pytz import utc
 
 bay_regex = re.compile(r'Bay\s+(\d+).*')
 bay_to_event = {}
 now = datetime.now().astimezone()
-print("Current time - ", now)
 
 ## Get current event from local calendar and only get events after it ends
-def get_existing_event(calendar_id: str, current_event_start: datetime, current_event_end: datetime, search_start: datetime, search_end: datetime):
+def does_event_exist(calendar_id: str, google_event: Event, start_time: datetime, end_time: datetime):
     local_events = []
     try:
-        start_str = search_start.astimezone(utc).strftime("%Y-%m-%dT%H:%M:%SZ")
-        end_str = search_end.astimezone(utc).strftime("%Y-%m-%dT%H:%M:%SZ")
-        url = f"{ha_url}/calendars/{calendar_id}?start={start_str}&end={end_str}"
-        print(url)
+        start_str = start_time.strftime("%Y-%m-%dT%H:%M:%SZ")
+        end_str = end_time.strftime("%Y-%m-%dT%H:%M:%SZ")
+        url = f"{ha_url}/calendars/{calendar_id}?start={start_str}&end={end_str}" 
         result = get(url, 
              headers={"Authorization": f"Bearer {ha_access_token}",
                       "Content-Type": "application/json"})
-        print(result.text)
         local_events = json.loads(result.text)
     except Exception as e:
         raise e
-    print("Local events - ", local_events)
     for event in local_events:
-        print(event['start']['dateTime'], event['end']['dateTime'])
         event_start = datetime.strptime(event['start']['dateTime'], "%Y-%m-%dT%H:%M:%S%z")
         event_end = datetime.strptime(event['end']['dateTime'], "%Y-%m-%dT%H:%M:%S%z")
-        if event_start <= current_event_start and event_end >= current_event_end:
-            print("Found existing event - ", event['summary'])
-            return event['summary']
-    print("No existing event found")
-    return None
+        if(event_start <= google_event.start and event_end >= google_event.end and event['summary'] == google_event.summary):
+            return True
+    return False
 
 def filter_events_greater_than_now(event: Event):
+    print(event.start.tzinfo)
+    print(now.tzinfo)
     return event.start > now
 
 def get_future_events(start_time: datetime, end_time: datetime, bay_number: int, calendar: GoogleCalendar):
@@ -51,7 +45,6 @@ def get_future_events(start_time: datetime, end_time: datetime, bay_number: int,
     if(events_by_bay.get(str(bay_number)) == None):
         return []
     ## if they exist, filter the ones that are greater than now
-    print("Filtering events greater than now - ", now)
     return list(filter(filter_events_greater_than_now, events_by_bay[str(bay_number)]))
 
 def get_events_by_bay(events: list[Event]):
@@ -78,10 +71,9 @@ def create_local_event(calendar_id: str, start_time: datetime, end_time: datetim
                           "end_date_time": end_time.strftime("%Y-%m-%d %H:%M:%S"),
                           "summary": summary,
                           "description": summary})
-    if response.status_code >= 200 and response.status_code < 300:
-        print("Created event - ", summary)
-    else:
-        print("ERROR in creating event - ", response.text)
+    print(response.text)
+    print(response.status_code)
+    print("Created event - ", summary)
         
 def main():
     global current_bay_number
@@ -111,6 +103,7 @@ def main():
 
     print(f"HA Access Token: {ha_access_token}")
     print(f"HA URL: {ha_url}")
+    print("Initializing Home Assistant Client")
 
     token = Credentials(
     token=config_data.get('google_access_token'),
@@ -121,44 +114,45 @@ def main():
     token_uri='https://oauth2.googleapis.com/token'
 )
     google_calendar = GoogleCalendar("primary",credentials=token)
-    current_time = now
-    end_of_day = datetime(year=now.year, month=now.month, day=now.day, hour=23, minute=59, second=0)
-    print("Fetching events from Google Calendar - ", current_time, " to ", end_of_day)
-    future_events = get_future_events(current_time,end_of_day, current_bay_number, google_calendar)
+    start_time = now
+    end_time = datetime(year=now.year, month=now.month, day=now.day, hour=23, minute=59, second=0)
+    print("Fetching events from Google Calendar - ", start_time, " to ", end_time)
+    future_events = get_future_events(start_time,end_time, current_bay_number, google_calendar)
     if(len(future_events) == 0):
         print("No events found")
         return
     
     # Currently no method exists to remove stale events from local calendar
-    start_time = future_events[0].start
-    end_time = future_events[0].end
-    summary = future_events[0].summary
-    for idx, event in enumerate(future_events):
-        if(idx == 0):
-            continue # First event, nothing to compare
-        # Extend the window if next event is about to overlap
-        if(event.start > start_time and event.start <= end_time ):
-            end_time = event.end
-            summary += " ," + event.summary
+    for event in future_events:
+        local_event_exists = does_event_exist(current_calendar_id, event, start_time, end_time)
+        if(local_event_exists):
+            print("Event for ", event.summary, " already exists. Skipping ....")
             continue
-        if(event.start > end_time):
-            local_event_summary = get_existing_event(current_calendar_id, start_time, end_time, current_time, end_of_day)
-            if local_event_summary is None:
-                print("Creating new event - ", summary, " Start time -",  start_time, " End time -", end_time)
-                create_local_event(current_calendar_id, start_time, end_time, summary)
-            else:
-                print("Event for ", local_event_summary, " already exists. Skipping ....")
-            start_time = event.start
-            end_time = event.end
-            summary = event.summary
-            continue
-    
-    local_event_summary = get_existing_event(current_calendar_id, start_time, end_time, current_time, end_of_day)
-    if local_event_summary is None:
-        print("Attempting to create new event - ", summary, " Start time -", start_time, " End time -", end_time)
-        create_local_event(current_calendar_id, start_time, end_time, summary)
-    else:
-        print("Event for ", local_event_summary, " already exists. Skipping ....")
+        create_local_event(current_calendar_id, event.start, event.end, event.summary)
+        print("Will create event for ", event.summary, " at ", event.start, " to ", event.end)
+
+    # Code to merge overlapping events
+    # start_time = future_events[0].start
+    # end_time = future_events[0].end
+    # for idx, event in enumerate(future_events):
+    #     if(idx == 0):
+    #         continue # First event, nothing to compare
+    #     # Extend the window if next event is about to overlap
+    #     # Todo add buffer time of 5 minutes
+    #     if(event.start > start_time and event.start <= end_time ):
+    #         end_time = event.end
+    #         continue
+    #     if(event.start > end_time):
+    #         print("Creating new event - ", start_time, "  ",  end_time)
+    #         create_local_event(current_calendar_id, start_time, end_time, "Hello")
+    #         start_time = event.start
+    #         end_time = event.end
+    #         continue
+    # print("Creating a new event - ", start_time, "  ", end_time)
+    # create_local_event(current_calendar_id, start_time, end_time, "Hello")
+
         
 if __name__ == "__main__":
     main()
+
+
